b8ad066fce983bdf881ed0f67731b7b4b400271c
Petro Ikonen <46808841+Ikope@users.noreply.github.com>
2022-04-26 20:20:08 +0300

    first production version done
    made funtionality to website

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,25 @@
 <template>
-  <div id="app">
+  <div id="app" :class="typeof weather.main != 'undefined' && weather.main.temp > 16 ? 'warm':''">
     <main>
       <div class="search-box">
-        <input type="text" class="search-bar" placeholder="search..."/>
+        <input 
+          type="text" 
+          class="search-bar" 
+          placeholder="search..."
+          v-model="query"
+          @keypress="fetchWeather"
+        />
       </div>
 
-      <div class="weather-wrap">
+      <div class="weather-wrap" v-if="typeof weather.main !='undefined'">
         <div class="location-box">
-          <div class="location">korpilahti, Suomi</div>
-          <div class="date">tiistai 26 huhtikuu 2022</div>
+          <div class="location">{{  weather.name  }}, {{ weather.sys.country }}</div>
+          <div class="date">{{dateBuilder()}}</div>
         </div>
         
         <div class="weather-box">
-          <div class="temp">11°c</div>
-          <div class="weather">sadetta</div>
+          <div class="temp">{{ Math.round(weather.main.temp) }}°c</div>
+          <div class="weather">{{ weather.weather[0].main }}</div>
         </div>
       </div>
     </main>
@@ -25,9 +31,39 @@ export default {
   name: 'App',
   data(){
     return{
-      api_key: "0b63a62c76729618ff270d4e0bcd6fc8"
+      api_key: "0b63a62c76729618ff270d4e0bcd6fc8",
+      url_base: "https://api.openweathermap.org/data/2.5/",
+      query:'',
+      weather:{}
+    }
+  },
+  methods:{
+    fetchWeather(e) {
+      if (e.key == "Enter") {
+        fetch(`${this.url_base}weather?q=${this.query}&units=metric&appid=${this.api_key}`)
+          .then(res => {
+            return res.json();
+          }).then(this.setResults);
+      }
+    },
+    setResults (results) {
+      this.weather = results;
+    },
+    dateBuilder ()  {
+      let d= new Date();
+      let months = ["January","February","March","April","May","June","July",
+      "August","September","October","November","December"];
+      let days =["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+      let day = days[d.getDay()];
+      let date = d.getDate();
+      let month = months[d.getMonth()];
+      let year = d.getFullYear();
+
+      return`${day} ${date} ${month} ${year}`;
     }
   }
+
 }
 </script>
 
@@ -46,6 +82,9 @@ font-family: Helvetica, sans-serif;
   background-size: cover;
   background-position: bottom;
   transition: 0.4s;
+}
+#app.warm{
+  background-image: url('./assets/warmbg.jpg');
 }
 main{
   min-height: 100vh;
